0035043f3aa9a58f5458f57c7e31847886b97b87
Butakov B I <butakovbi3@gmail.com>
2022-10-31 23:01:05 +0300

    Contest_MasAndStrings_ButakovBI

---
+++ ButakovBI/Задачи с контеста Массивы, строки/B.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+int main() {
+
+  int n = 0;
+  std::cin >> n;
+  std::vector<int> cafeteria_v;
+  std::set<int> cafeteria_set;
+
+  for (int i = 0; i < n; ++i) {
+    int a = 0;
+    std::cin >> a;
+    cafeteria_set.insert(a);
+    cafeteria_v.push_back(a);
+  }
+
+
+  int ans = -1;
+  std::set<int> new_set;
+  for (int j = n - 1; j >= 0; --j) {
+    new_set.insert(cafeteria_v[j]);
+    if (new_set.size() == cafeteria_set.size()) {
+      ans = cafeteria_v[j];
+      break;
+    }
+  }
+  if (ans == -1) {
+    ans = cafeteria_v[0];
+  }
+  std::cout << ans << "\n";
+  return 0;
+}
---
+++ ButakovBI/Задачи с контеста Массивы, строки/G.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main() {
+  int n;
+  std::cin >> n;
+
+  std::string s;
+  std::cin >> s;
+
+  std::vector<int> counts(26);
+  for (char c : s) {
+    ++counts[tolower(c) - 'a'];
+  }
+
+  bool result = none_of(counts.begin(), counts.end(), [&](int c) {return c == 0; });
+
+  //bool result = true;
+  //for (int element : counts) {
+  // if (element == 0) {
+  //      result = false;
+  // }
+  //}
+
+  std::cout << (result ? "YES" : "NO");
+
+}
