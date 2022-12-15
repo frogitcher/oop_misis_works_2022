cdad3d62b09ea3c8a3dd84775bdef92c94af7389
mrapplexz <mr.applexz@gmail.com>
2022-12-15 17:48:49 +0300

    use simpson's formulae for task c

--- AfanasevMA/contest3/c.cpp
+++ AfanasevMA/contest3/c.cpp
@@ -3,19 +3,29 @@
 #include <iomanip>
 
 
-#define PI std::acos(-1)
-
-
-double cap_volume(double width, double height, double height_cap) {
-    double h = height_cap;
-    double a = width / 2;
-    double b = width / 2;
-    double c = height / 2;
-    return ((PI * a * b) / (3 * c * c)) * h * h * (3 * c - h);
+double CircleRadiusAtZ(double z, double height, double width) {
+    double ab = (width / 2);
+    double c = (height / 2);
+    return std::sqrt((ab * ab) * (1 - (z * z) / (c * c)));
 }
 
+double PartVolume(int steps, double height, double width, double height_from, double height_to) {
+    // интересно, зачем гомер симпсон придумал интеграл?
+    double pi = 3.14159265359;
+    double area_sum = 0;
+    double z_from = (height / 2) - height_to;
+    double z_to = (height / 2) - height_from;
+    double z_step_value = (z_to - z_from) / steps;
+    for (int i = 0; i <= steps; i++) {
+        double z_at = z_from + z_step_value * i;
+        double radius = CircleRadiusAtZ(z_at, height, width);
+        double area = pi * radius * radius;
+        area_sum += area;
+    }
+    return ((height_to - height_from) / steps) * area_sum;
+}
 
-double round_to(double value, int decimal_places) {
+double RoundTo(double value, int decimal_places) {
     double precision = std::pow(10.0, -decimal_places);
     return std::round(value / precision) * precision;
 }
@@ -26,11 +36,9 @@ int main() {
     double a, b;
     int n;
     std::cin >> a >> b >> n;
-    double prev_cap_vol = 0;
     for (int i = 0; i < n; i++) {
-        double cap_vol = cap_volume(a, b, (b / n) * (i + 1));
-        std::cout << round_to(cap_vol - prev_cap_vol, 6) << std::endl;
-        prev_cap_vol = cap_vol;
+        double area = PartVolume(10000000, b, a, (b / n) * (i), (b / n) * (i + 1));
+        std::cout << RoundTo(area, 6) << std::endl;
     }
 
 }
