e9b66a68102eac1f54b8cda48f4638db8bc46b30
RomBurunduk <47496016+RomBurunduk@users.noreply.github.com>
2022-12-16 18:53:36 +0300

    working version

--- number methods/ananas/main.cpp
+++ number methods/ananas/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 #include <cmath>
-using namespace std;
 
-double integral(double x, double a, double b){
-    return (b*b*x - (b*b*x*x*x)/(3*a*a));
+double y(double a, double b, double x){
+    return b*sqrt(1 - pow(x,2) / pow(a,2));
 }
 
 int main() {
     const double PI = 3.141592653589793;
-    double a,b,j;
-    int n;
-    cin>>b>>a>>n;
-    double wid=a/n;
+    double a,b;
+    int n, iter=1000000;
+    std::cout<<std::fixed<<std::setprecision(6);
+    std::cin>>b>>a>>n;
+    double width = a/n;
     a/=2;
     b/=2;
-    std::cout.precision(6);
+    double subwidth = width/iter;
+
     for (int i=0; i<n; i++) {
-        j = a-i*wid;
-        std::cout<<std::fixed<<round(((integral(j,a,b)-integral(j-wid,a,b))*PI)*1000000)/1000000<<std::endl;
+        double left = a-width*i;
+        double pos = left - subwidth/2.0;
+        double volume = 0;
+        for (int j=0; j<iter; j++) {
+            volume += PI*pow(y(a, b, pos),2)*subwidth;
+            pos-=subwidth;
+        }
+        std::cout<<volume<<std::endl;
     }
 }
