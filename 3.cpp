0e2f748faef5d3e56f6894fe500fdc97542e832c
stepalikh <Likhodedov.stepan@yandex.com>
2022-12-17 16:13:36 +0300

    KR161

---
+++ 3.cpp
@@ -0,0 +1,39 @@
+﻿#include "3.h"
+#include <iostream>
+#include <set>
+#include <list>
+
+std::set<int> InsertIntoFirst(int value, std::set<int> massive)
+{
+	massive.insert(value);
+	return massive;
+}
+
+std::set<int> InsertIntoSecond(int value, std::set<int> massive)
+{
+	massive.insert(value);
+	return massive;
+}
+
+std::set<int> EraseFromFirst(int value, std::set<int> massive)
+{
+	massive.erase(value);
+	return massive;
+}
+
+std::set<int> EraseFromSecond(int value, std::set<int> massive)
+{
+	massive.erase(value);
+	return massive;
+}
+
+int main()
+{
+	std::set<int> a;
+	std::set<int> b;
+	InsertIntoFirst(5, a);
+	InsertIntoSecond(2, b);
+	EraseFromFirst(5, a);
+	EraseFromSecond(2, b);
+	return 0;
+}
