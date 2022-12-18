2004103d57d04b9eaf8f9a599fe03db76654ea13
Eduard <85809350+FRidere@users.noreply.github.com>
2022-12-18 23:22:16 +0300

    Update third.cpp

--- Pochuev/contest3/third.cpp
+++ Pochuev/contest3/third.cpp
@@ -3,52 +3,77 @@
 #include<vector>
 #include<iomanip>
 
+//const double Pi = std::atan(1) * 4;
 const double Pi = 3.14159265358979;
-double Cup_volume(double a, double b,double c){
-    return Pi * a * a* c*c * (3*b - c) / (3 * b* b);
+
+double Radius(double x, double a, double b) {
+    return std::max(0.0,std::sqrt(b*b - b*b * x * x / (a*a)));
 }
 
-int main(){
+int main() {
+    std::cout << std::fixed << std::setprecision(10);
     int n;
-    double a,b;
-    std::cin >> a >> b >> n;
-    
-    double delta = a / n;
+    double width, height;
+    std::cin >> width >> height >> n;
+    double count = n / 2 + n % 2;
+    double length = height / n;
+    int iterations = 1400000;
+    double delta = length / iterations;
+    std::vector<double> ans;
 
-    if(n == 1)
-        std::cout << std::fixed<<std::setprecision(6)<< 4 * Pi * a* a* b /24;
-    else if(n % 2 == 0){
-        double prev = Cup_volume(a/2,b/2,b/2);
-        std::vector<double> ans;
-
-        for(int i = n/2 - 1; i >= 0; --i) {
-            double cur = Cup_volume(a / 2, b / 2, b * i / n);
-            ans.push_back(std::abs(prev - cur));
-            prev = cur;
+    if (n % 2 == 0) {
+        for (int i = 0; i < count; ++i) {
+            double volume = 0;
+            double left_limit = length * i;
+            for (int j = 0; j < iterations; j++) {
+                double left_x = left_limit + delta * j;
+                double right_x = left_limit + delta * (j + 1);
+                double left_r = Radius(left_x, height / 2, width / 2);
+                double right_r = Radius(right_x, height / 2, width / 2);
+                volume += Pi * delta * (left_r * left_r + left_r * right_r + right_r * right_r) / 3;
+            }
+            ans.push_back(volume);
         }
-        for(auto i = ans.rbegin(); i != ans.rend(); ++i)
-            std::cout << std::fixed<<std::setprecision(6)<< *i<< '\n';
-        for(auto i:ans)
-            std::cout << std::fixed<<std::setprecision(6)<<i << '\n';
-    }
-    else{
-        std::vector<double>ans;
-        double middle_elem = Cup_volume(a/2,b/2,b/2) - Cup_volume(a/2,b/2,b/2 - b/n/2);
-    
-        ans.push_back(middle_elem * 2);
-
-        double prev = Cup_volume(a/2,b/2, b/2 - b/n/2);
-
-        for(int i = (n-2)/2; i >= 0; --i){
-            double cur = Cup_volume(a/2,b/2,b* i/n);
-            ans.push_back(std::abs(prev - cur));
-            prev = cur;
+        for (auto it = ans.rbegin(); it < ans.rend(); ++it) {
+            std::cout << *it << '\n';
         }
-       for(auto i = ans.rbegin(); i != ans.rend(); ++i)
-            std::cout << std::fixed<<std::setprecision(6)<< *i<< '\n';
-       for(auto i = ans.begin()+1;i<ans.end();++i)
-            std::cout << std::fixed<<std::setprecision(6)<< *i << '\n';
-    }
+        for (auto it = ans.begin(); it < ans.end(); ++it) {
+            std::cout << *it << '\n';
+        }
 
+    }
+    else {
+        double aver_elem = length / 2;
+        double volume = 0;
+        delta /= 2;
+        for (int i = 0; i < iterations; i++) {
+            double left_x = delta * i;
+            double right_x = delta * (i + 1);
+            double left_r = Radius(left_x, height / 2, width / 2);
+            double right_r = Radius(right_x, height / 2, width / 2);
+            volume += Pi * delta * (left_r * left_r + left_r * right_r + right_r * right_r) / 3;
+        }
+        ans.push_back(volume * 2);
+        delta *= 2;
+        for (int i = 0; i < count - 1; ++i) {
+            double volume = 0;
+            double left_limit = aver_elem + length * i;
+            for (int j = 0; j < iterations; j++) {
+                double left_x = left_limit + delta * j;
+                double right_x = left_limit + delta * (j + 1);
+                double left_r = Radius(left_x, height / 2, width / 2);
+                double right_r = Radius(right_x, height / 2, width / 2);
+                volume += Pi * delta * (left_r * left_r + left_r * right_r + right_r * right_r) / 3;
+            }
+            ans.push_back(volume);
+        }
+        for (auto it = ans.rbegin(); it < ans.rend(); ++it) {
+            std::cout << *it << '\n';
+        }
+        for (auto it = ans.begin() + 1; it < ans.end(); ++it) {
+            std::cout << *it << '\n';
+        }
+
+    }
     return 0;
 }
