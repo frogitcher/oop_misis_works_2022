7cae441f07d808b7b51989f92acb46f182f918b1
Eduard <85809350+FRidere@users.noreply.github.com>
2022-12-18 23:14:32 +0300

    Update Rational.cpp
    в визуалке есть функция автозаписывания реализации метода из хэдер файла в cpp( сложно описать словами, проще показать ), и почему-то в этой реализации скобки ставятся на следующей строке, а я и не заметил этого.

--- Pochuev/classRational/Rational.cpp
+++ Pochuev/classRational/Rational.cpp
@@ -5,8 +5,7 @@ std::ostream& operator << (std::ostream& stream, const Rational& obj) {
 		return stream << obj.GetNum();
 	return stream << obj.asString();
 }
-std::istream& operator>>(std::istream&stream,  Rational&obj)
-{
+std::istream& operator>>(std::istream&stream,  Rational&obj){
 		int a, b;
 		char tmp;
 		stream >>a >> tmp >> b;
@@ -27,13 +26,11 @@ bool operator<(const double& lhs, const Rational& rhs) {
 	return lhs < rhs.asDouble();
 }
 
-bool operator>=(const double& lhs, const Rational& rhs)
-{
+bool operator>=(const double& lhs, const Rational& rhs){
 	return lhs > rhs or lhs == rhs;
 }
 
-bool operator<=(const double& lhs, const Rational& rhs)
-{
+bool operator<=(const double& lhs, const Rational& rhs){
 	return lhs < rhs or lhs == rhs;
 }
 
@@ -41,35 +38,30 @@ bool operator==(const double& lhs, const Rational& rhs) {
 	return lhs == rhs.asDouble();
 }
 
-bool operator!=(const double& lhs, const Rational& rhs)
-{
+bool operator!=(const double& lhs, const Rational& rhs){
 	return lhs != rhs.asDouble();
 }
 
-void Rational::Normalise()
-{
+void Rational::Normalise(){
 	if (num < 0 and den < 0) { num *= -1; den *= -1; }
 	int g = gcd(std::abs(num), std::abs(den));
 	num /= g;
 	den /= g;
 }
 
-Rational::Rational(const Rational& tmp)
-{
+Rational::Rational(const Rational& tmp){
 	this->den = tmp.GetDen();
 	this->num = tmp.GetNum();
 	//std::cout << "copied";
 }
 
-Rational& Rational::operator=(const Rational& rhs)
-{
+Rational& Rational::operator=(const Rational& rhs){
 	this->den = rhs.GetDen();
 	this->num = rhs.GetNum();
 	return *this;
 }
 
-Rational& Rational::operator=(Rational&&rhs)
-{
+Rational& Rational::operator=(Rational&&rhs){
 	num = rhs.num;
 	den = rhs.den;
 	rhs.num = 0;
@@ -81,121 +73,98 @@ Rational Rational::operator+(const Rational& rhs) const {
 	return Rational(this->num * rhs.GetDen() + rhs.GetNum() * this->den, this->den * rhs.GetDen());
 }
 
-Rational& Rational::operator+=(const Rational& rhs)
-{
+Rational& Rational::operator+=(const Rational& rhs){
 	return *this = *this + rhs;
 }
 
-Rational Rational::operator-(const Rational& rhs) const
-{
+Rational Rational::operator-(const Rational& rhs) const{
 	return Rational(this->GetNum()*rhs.GetDen() - rhs.GetNum()*this->GetDen(), this->GetDen()*rhs.GetDen());
 }
 
-Rational& Rational::operator-=(const Rational&rhs)
-{
+Rational& Rational::operator-=(const Rational&rhs){
 	return *this = *this - rhs;
 }
 
-Rational Rational::operator*(const Rational&rhs) const
-{
+Rational Rational::operator*(const Rational&rhs) const{
 	return Rational(rhs.GetNum()*this->num,this->den * rhs.GetDen());
 }
 
-Rational& Rational::operator/=(const Rational&rhs)
-{
+Rational& Rational::operator/=(const Rational&rhs){
 	return *this = *this / rhs;
 }
 
-Rational& Rational::operator++()
-{
+Rational& Rational::operator++(){
 	return *this += 1;
 }
 
-Rational& Rational::operator++(int)
-{
+Rational& Rational::operator++(int){
 	Rational& temp = *this;
 	*this += 1;
 	return temp;
 }
 
-Rational& Rational::operator--()
-{	
+Rational& Rational::operator--(){	
 	return *this -= 1;
 }
 
-Rational& Rational::operator--(int)
-{
+Rational& Rational::operator--(int){
 	Rational& temp = *this;
 	*this -= 1;
 	return temp;
 }
 
-bool Rational::operator==(const Rational&rhs)const
-{
+bool Rational::operator==(const Rational&rhs)const{
 	return (this->GetDen() == rhs.GetDen())and (this->GetNum() == rhs.GetNum());
 }
 
-bool Rational::operator!=(const Rational&rhs)const
-{
+bool Rational::operator!=(const Rational&rhs)const{
 	return (this->GetDen() != rhs.GetDen()) or (this->GetNum() != rhs.GetNum());
 }
 
-bool Rational::operator>(const Rational&rhs)const
-{
+bool Rational::operator>(const Rational&rhs)const{
 	return (*this - rhs).GetNum() > 0;
 }
 
-bool Rational::operator<(const Rational&rhs) const
-{
+bool Rational::operator<(const Rational&rhs) const{
 	return (*this - rhs).GetNum() < 0;
 }
 
-bool Rational::operator>=(const Rational&rhs) const
-{
+bool Rational::operator>=(const Rational&rhs) const{
 	return *this > rhs or *this == rhs;
 }
 
-bool Rational::operator<=(const Rational&rhs) const
-{
+bool Rational::operator<=(const Rational&rhs) const{
 	return *this < rhs or *this == rhs;
 }
 
-bool Rational::operator==(const double&rhs) const
-{
+bool Rational::operator==(const double&rhs) const{
 	return this->asDouble() == rhs;
 }
 
-bool Rational::operator!=(const double&rhs) const
-{
+bool Rational::operator!=(const double&rhs) const{
 	return this->asDouble() != rhs;
 }
 
-bool Rational::operator>(const double&rhs) const
-{
+bool Rational::operator>(const double&rhs) const{
 	return this->asDouble() > rhs;
 }
 
-bool Rational::operator<(const double&rhs) const
-{
+bool Rational::operator<(const double&rhs) const{
 	return this->asDouble() < rhs;
 }
 
-bool Rational::operator<=(const double&rhs) const
-{
+bool Rational::operator<=(const double&rhs) const{
 	return this->asDouble() <= rhs;
 }
 
-bool Rational::operator>=(const double&rhs) const
-{
+bool Rational::operator>=(const double&rhs) const{
 	return this->asDouble() >= rhs;
 }
 
-Rational Rational::operator/(const Rational&rhs)const
-{
+Rational Rational::operator/(const Rational&rhs)const{
 	return Rational(this->num * rhs.GetDen(), this->den * rhs.GetNum());
 }
 
-Rational& Rational::operator*=(const Rational&rhs) 
-{
+Rational& Rational::operator*=(const Rational&rhs) {
 	return *this = *this * rhs;
 }
